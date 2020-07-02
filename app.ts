46383afda80738690a553cc3984796ee38cdc3a5
Brice Friha <brice.friha@aol.fr>
2020-07-02 22:35:44 +0200

    First commit

---
+++ app.ts
@@ -0,0 +1,15 @@
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+import router from './routes.ts';
+
+// Set port and host variable
+const HOST = '127.0.0.1';
+const PORT = 7700;
+
+// 
+const app = new Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Listening on port ${PORT} ...`);
+await app.listen(`${HOST}:${PORT}`);
